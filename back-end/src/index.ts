94d6eb6036d37dc2dc3fdd523d2042ee8bd74d9c
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-06-30 17:40:11 -0300

    fix: teste pull request

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    return res.status(200).json('Olá Mundo! Essa é a minha primeira rota :)');
+    return res.status(200).json(' Hello world :) ');
 });
 
 const PORT = 3030;

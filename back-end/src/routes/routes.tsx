a7985896211d143ce3ca03518d2861370a66e8ab
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-07-05 16:29:12 -0300

    feat: add route getAllProducts

--- back-end/src/routes/routes.tsx
+++ back-end/src/routes/routes.tsx
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import multerConfig from '../config/multer'
 import PostProduct from '../services/productServices/postProduct/postProduct';
 import DeleteProduct from '../services/productServices/deleteProduct/deleteProdutc';
+import GetProducts from '../services/productServices/getProduct/getAll';
 const routes = Router();    
 const upload = multer(multerConfig);
 
@@ -13,6 +14,8 @@ routes.post('/postProduct', upload.any(), (req, res) => {
 routes.delete('/deleteProduct/:id', (req, res) => {
     DeleteProduct.deleteProduct(req, res);
 });
+
+routes.get('/getProducts', GetProducts.getAll);
 
 export { routes };
 

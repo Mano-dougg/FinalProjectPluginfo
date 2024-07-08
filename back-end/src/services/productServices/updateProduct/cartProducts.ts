9da76dbd7bad550ed80752cf5c7f237eb2724ae3
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-07-08 17:10:45 -0300

    fix: refactoring code

--- back-end/src/services/productServices/updateProduct/cartProducts.ts
+++ back-end/src/services/productServices/updateProduct/cartProducts.ts
@@ -5,6 +5,12 @@ const prisma = new PrismaClient();
 
 class Cart {
 
+     /**
+     * Recebe o ID e o novo valor da quantidade carrinho e atualiza no banco
+     * @param {Request} req - Objeto de solicitação do Express
+     * @param {Response} res - Objeto de resposta do Express
+     * @returns {Promise<void>} 
+     */
     static async updateCartProducts (req: Request, res: Response) {
         const { id } = req.params;
         const { quantidade_carrinho } = req.body
@@ -13,9 +19,7 @@ class Cart {
             let produto = await prisma.produto.findUnique({where: { id: Number(id) }});
 
             // CHECK IF PRODUCT EXISTS
-            if (!produto) {
-                return res.status(422).json({ msg: "Não foi possível encontrar esse membro"})
-            }
+            if (!produto) { return res.status(422).json({ msg: "Nenhum produto foi encontrado"}) }
 
             produto = await prisma.produto.update({ 
                 where: { id: Number(id) },
@@ -28,7 +32,7 @@ class Cart {
             return res.status(200).json({ msg: "Carrinho atualizado com sucesso"});
         } catch (error) {
             console.error("Erro ao adicionar ao carrinho", error);
-            res.status(500).json({ msg: "Aconteceu um erro no servidor, tente novamente mais tarde" });
+            res.status(500).json({ msg: "Erro ao adicionar ao carrinho" });
         }
     }
 }

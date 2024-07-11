3f9771fef01854403dc8fe4a4ba93853e327941e
douglas aleixo <douglas.aleixo2806@gmail.com>
2024-07-10 23:56:41 -0300

    fix: altering architecture

--- back-end/entities/user.ts
+++ back-end/entities/user.ts
@@ -1,8 +1,21 @@
-interface User {
-    id: number;
-    name: string;
-    email: string;
-    senha: string;
+import { uuid } from 'uuidv4';
+
+class User {
+    public readonly id: string;
+
+    public nome: string;
+    public email: string;
+    public senha: string;
+
+    constructor(props: Omit<User, 'id'>, id?: string) {
+        Object.assign(this, props);
+
+
+        if (!id) {
+            this.id = uuid();
+        }
+    }
+
 }
 
 export default User;
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,13 +1,5 @@
-import express from 'express';
-import { routes } from './routes/routes';
+import { app } from './app'
 require('dotenv').config();
-
-const cors = require('cors');
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(routes);
 
 const PORT = 3030;
 

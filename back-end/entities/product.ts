eeb8921a399abc6312aa89b0234c595f1216095e
EderNatan.dev <edernatan124@gmail.com>
2024-07-12 21:51:38 -0300

    add cors

--- back-end/entities/product.ts
+++ back-end/entities/product.ts
@@ -1,19 +1,24 @@
-interface Product {
-    id: number,
-    nome: string,
-    marca: string,
-    preco: number,
-    preco_alterado: number,
-    promocao: number,
-    descricao: string,
-    quantidade_carrinho: number,
-    face: boolean,
-    labios: boolean,
-    olhos: boolean,
-    kits: boolean,
-    sombrancelha: boolean,
-    unhas: boolean,
-    original: boolean,
+class Product { 
+    public id: number;
+    public nome: string;
+    public marca: string;
+    public preco: number;
+    public preco_alterado: number;
+    public promocao: number;
+    public descricao: string;
+    public quantidade_carrinho: number;
+    public face: boolean;
+    public labios: boolean;
+    public olhos: boolean;
+    public kits: boolean;
+    public sombrancelha: boolean;
+    public unhas: boolean;
+    public original: boolean;
+
+
+    constructor(props: Omit<Product, 'id'>, id?: string) {
+        Object.assign(this, props);
+    }
 }
 
 export default Product;
